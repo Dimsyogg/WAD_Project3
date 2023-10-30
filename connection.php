7f4ddcfefbdb8613cefba415c4a28e18b5932865
Dimsyogg <106477675+Dimsyogg@users.noreply.github.com>
2023-10-30 14:21:29 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+//koneksi ke database
+$conn = mysqli_connect("localhost","root","","wad_project3");
+$result = mysqli_query($conn,"SELECT * FROM students");
+
+// var_dump($result);
+echo "<br/>";
+var_dump(mysqli_fetch_object($result));
+?>
