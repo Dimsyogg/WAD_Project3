982fb9af52b2d227f8069ab382fd331d6ba4cdd1
Dimsyogg <106477675+Dimsyogg@users.noreply.github.com>
2023-10-30 16:31:30 +0700

    Update connection.php
    hapus komen

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $conn = mysqli_connect("localhost","root","","wad_project3");
 $result = mysqli_query($conn,"SELECT * FROM students");
 
-// var_dump($result);
+var_dump($result);
 echo "<br/>";
 var_dump(mysqli_fetch_object($result));
 ?>
